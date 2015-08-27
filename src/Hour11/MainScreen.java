1c6b2ca6dde50124f744fc623e548ea6f2e154d6
loitv <loitran1763@gmail.com>
2015-08-27 19:35:28 +0700

    Hour 10 and 11

--- src/Hour11/MainScreen.java
+++ src/Hour11/MainScreen.java
@@ -2,4 +2,13 @@ package Hour11;
 
 public class MainScreen {
 
+	public static void main(String[] args) {
+		
+		Virus influenza = new Virus();
+		influenza.newSecond = 92;
+		influenza.maxFileSize = 40000;
+		//Virus.virusCount++;
+		
+		
+	}
 }
--- src/Hour11/Virus.java
+++ src/Hour11/Virus.java
@@ -2,4 +2,8 @@ package Hour11;
 
 public class Virus {
 
+	public int newSecond = 86;
+	public String author = "Loitv";
+	int maxFileSize = 30000;
+	static int virusCount = 0;
 }
